0bfb195adee358bedd4716fe0c66d1c0a2a47061
Miklos Nagy <miklos.mnagy@gmail.com>
2024-06-18 15:27:12 +0200

    Update deforum_iteration_nodes.py
    fix next_prompt issue

--- deforum_nodes/nodes/deforum_iteration_nodes.py
+++ deforum_nodes/nodes/deforum_iteration_nodes.py
@@ -211,8 +211,8 @@ class DeforumIteratorNode:
 
         self.args = args
         self.root = root
-        if prompt_series is not None:
-            gen_args["next_prompt"], _ = split_weighted_subprompts(next_prompt)
+        if next_prompt is not None:
+            gen_args["next_prompt"], _ = split_weighted_subprompts(str(next_prompt))
             gen_args["prompt_blend"] = blend_value
         gen_args["frame_index"] = self.frame_index
         gen_args["max_frames"] = anim_args.max_frames
